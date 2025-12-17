d41afe79e8a14687000bf4e2e9b757d0693aa979
mucahit64 <mucahit.yaman@hotmail.com>
2025-12-17 23:59:55 +0300

    Add nitro preset for Railway

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['nuxt-quasar-ui'],
+
+  nitro: {
+    preset: 'node-server',
+  },
 })
